5e5f84345290ab4817e7d410899ba9ab9f4ad55d
Moeen Mahmud <write2fahim007@gmail.com>
2021-11-03 18:50:19 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Client
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@doctorsportalcluster.8qkfl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri);
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("Client connected successfully");
+  } finally {
+    // client.close()
+  }
+}
+
+run().catch(console.dir);
+
+// Server running
+app.get("/", (req, res) => {
+  res.send("Hello Doctors Portal!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
