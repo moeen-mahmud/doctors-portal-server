8a82fdfee2b98a537f4acb1ef8e32442c9ac9725
Moeen Mahmud <write2fahim007@gmail.com>
2021-11-07 13:41:45 +0600

    added role for admin

--- index.js
+++ index.js
@@ -79,6 +79,18 @@ async function run() {
       const result = await usersCollection.updateOne(filter, updateDoc);
       res.json(result);
     });
+
+    // Check admin role
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let isAdmin = false;
+      if (user?.role === "admin") {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
+    });
   } finally {
     // client.close()
   }
